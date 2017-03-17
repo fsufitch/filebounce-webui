0134ce903600c8a321054108c3aebdc37ab3da42
Filip Sufitchi <fsufitchi@gmail.com>
2017-03-16 22:11:26 -0400

    Link to "production" transfer node for prod builds

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -18,6 +18,10 @@ module.exports = webpackMerge(webpackCommon, {
   plugins: [
     new webpack.DefinePlugin({
       __PROD__: true,
+      'process.env': {
+        TRANSFER_NODE_DOWNLOAD_URL: '"http://opensourcenerd.com:8888/download"',
+        TRANSFER_NODE_URL: '"ws://opensourcenerd.com:8888/client_ws"',
+      }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
